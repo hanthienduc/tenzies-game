52693364aa2e498ef7ecc1b6896f73ca842cfc1d
hanthienduc <hanthienduc.96@gmail.com>
2022-07-17 19:03:35 +0900

    modified access database

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ import Confetti from "react-confetti";
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
-
   const [tenzies, setTenzies] = React.useState(false);
+  const [playername, setPlayername] = React.useState("");
+  const [message, setMessage] = React.useState({});
+  const [displayData, setDisplayData] = React.useState({});
 
   function generateNewDie() {
     return {
@@ -27,8 +29,9 @@ function App() {
 
   function rollNewDice(event, id) {
     if (tenzies) {
-      return setDice(allNewDice());
       setTenzies(false);
+      handleAddScore(event);
+      setDice(allNewDice());
     }
 
     setDice((prevState) => {
@@ -56,6 +59,10 @@ function App() {
     const firstValue = dice[0].value;
     const allSameValue = dice.every((item) => item.value === firstValue);
     if (allHeld && allSameValue) {
+      setDisplayData((prevData) => ({
+        ...prevData,
+        score: prevData.score + 1,
+      }));
       setTenzies((prevTenzies) => !prevTenzies);
     }
   }, [dice]);
@@ -69,9 +76,123 @@ function App() {
     />
   ));
 
+  function handleChange(event) {
+    const { value } = event.target;
+    setPlayername(value);
+  }
+
+  let handleAddNewPlayer = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://tenzie-project.vercel.app/api/add-new-player", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: playername,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 404) {
+        setMessage((prevMessage) => {
+          return {
+            content: resJson.message,
+            type: !prevMessage.type,
+          };
+        });
+      }
+      if (res.status === 200) {
+        setPlayername("");
+        setMessage((prevMessage) => {
+          return {
+            type: !prevMessage.type,
+            content: resJson.message,
+          };
+        });
+        setTimeout(() => {
+          setMessage({});
+        }, 2000);
+        setDisplayData((prevData) => {
+          return {
+            score: 0,
+            name: resJson.name,
+          };
+        });
+      }
+    } catch (err) {}
+  };
+
+  React.useEffect(() => {
+    const getDisplayData = JSON.parse(localStorage.getItem("displaydata"));
+    if (getDisplayData) {
+      setDisplayData(getDisplayData);
+    }
+  }, []);
+
+
+  React.useEffect(() => {
+    if (displayData.name) {
+      localStorage.setItem("displaydata", JSON.stringify(displayData));
+    }
+  }, [displayData]);
+
+  let handleAddScore = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://tenzie-project.vercel.app/api/add-score", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          score: displayData.score,
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+        setMessage((prevMessage) => {
+          return {
+            type: !prevMessage.type,
+            content: resJson.message,
+          };
+        });
+        setTimeout(() => {
+          setMessage({});
+        }, 2000);
+        setDisplayData((prevData) => {
+          return {
+            ...prevData,
+            score: 0,
+          };
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <main className="app">
       {tenzies && <Confetti />}
+      {message.content && (
+        <p className={!message.type ? "error" : ""}>{message.content}</p>
+      )}
+      {displayData.name ? (
+        <div className="display-data">
+          <h4>Name: {displayData.name}</h4>
+          <h4>Score: {displayData.score}</h4>
+        </div>
+      ) : (
+        <form onSubmit={handleAddNewPlayer} className="form">
+          <input
+            name="name"
+            placeholder="Player name?"
+            value={playername}
+            onChange={handleChange}
+          />
+          <button type="submit" className="btn-create">
+            Create
+          </button>
+        </form>
+      )}
+
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. Click each die to freeze it at its
